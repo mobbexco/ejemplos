eea672da9683354f81b5183fa8153ecba8c20fc0
Ezekiel Grosfeld <ezekiel.grosfeld@mobbex.com>
2021-04-19 16:11:56 -0300

    Updating packages and minor fixes

--- embed/index.js
+++ embed/index.js
@@ -1,60 +1,61 @@
-const express = require('express')
-const mobbex = require('mobbex')
-const config = require('./config');
+const express = require("express");
+const mobbex = require("mobbex");
+const config = require("./config");
 
-let app = express()
+let app = express();
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + "/views"));
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 mobbex.configurations.configure({
-    apiKey: config.apiKey,
-    accessToken: config.accessToken
-})
+  apiKey: config.apiKey,
+  accessToken: config.accessToken,
+});
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.post('/checkout', (req, res) => {
-    const data = req.body
-    console.log(data)
-    const checkout = {
-        total: 100,
-        currency: "ARS",
-        reference: config.reference,
-        description: "Venta de 2 productos",
-        items: [
-            {
-                image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
-                quantity: 1,
-                description: data.product1Title,
-                total: parseInt(data.product1Val)
-            },
-            {
-                image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
-                quantity: 1,
-                description: data.product2Title,
-                total: parseInt(data.product2Val)
-            }
-        ],
-        options: {
-            domain: config.domain,
-            button: true,
-            embed: true
-        },
-        test: true,
-        return_url: "https://localhost:8443/sale/return?session=56789",
-    }
+app.post("/checkout", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  const checkout = {
+    total: 100,
+    currency: "ARS",
+    reference: config.reference,
+    description: "Venta de 2 productos",
+    items: [
+      {
+        image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
+        quantity: 1,
+        description: data.product1Title,
+        total: parseInt(data.product1Val),
+      },
+      {
+        image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
+        quantity: 1,
+        description: data.product2Title,
+        total: parseInt(data.product2Val),
+      },
+    ],
+    options: {
+      domain: config.domain,
+      button: true,
+      embed: true,
+    },
+    test: true,
+    return_url: "https://localhost:8443/sale/return?session=56789",
+  };
 
-    mobbex.checkout.create(checkout)
-        .then(data => res.status(200).send(data))
-        .catch(() => res.status(500))
-})
+  mobbex.checkout
+    .create(checkout)
+    .then((data) => res.status(200).send(data))
+    .catch(() => res.status(500));
+});
 
-app.listen(config.port)
+app.listen(config.port);
 
-console.log(`Running on port: ${config.port}`)
+console.log(`Running on port: ${config.port}`);
--- wallet/index.js
+++ wallet/index.js
@@ -1,77 +1,80 @@
-const express = require('express')
-const path = require('path');
-const mobbex = require('mobbex')
-const config = require('./config')
+const express = require("express");
+const { default: mobbex } = require("mobbex");
+const config = require("./config");
 
-let app = express()
+let app = express();
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+mobbex.configurations.configure({
+  apiKey: config.apiKey,
+  accessToken: config.accessToken,
+});
 
-app.post('/checkout', (req, res) => {
-    // Data del formulario del carrito
-    const data = req.body
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-    // Configuración de la clave privada
-    mobbex.configurations.setPrivateKey(config.privateKey)
+app.post("/checkout", (req, res) => {
+  // Data del formulario del carrito
+  const data = req.body;
 
-    // Objeto del checkout
-    const checkout = {
-        total: parseInt(data.total),
-        currency: "ARS",
-        reference: config.reference,
-        description: "Venta de 2 productos",
-        items: [
-            {
-                image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
-                quantity: 1,
-                description: data.product1Title,
-                total: parseInt(data.product1Val)
-            },
-            {
-                image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
-                quantity: 1,
-                description: data.product2Title,
-                total: parseInt(data.product2Val)
-            }
-        ],
-        customer: config.customer,
-        options: {
-            domain: config.domain
-        },
-        transparent: false,
-        test: true,
-        wallet: true,
-        return_url: "https://localhost:8443/sale/return?session=56789",
-        webhook: "https://localhost:8443/sale/webhook?user=1234"
-    }
+  // Configuración de la clave privada
 
-    // Creación del checkout
-    mobbex.checkout.create(checkout)
-        .then(data => {
-            // Wallet con todas las tarjetas
-            let wallet = data.data.wallet
-            // Url del checkout para agregar nuevas tarjetas
-            let url = data.data.url
+  // Objeto del checkout
+  const checkout = {
+    total: parseInt(data.total),
+    currency: "ARS",
+    reference: config.reference,
+    description: "Venta de 2 productos",
+    items: [
+      {
+        image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
+        quantity: 1,
+        description: data.product1Title,
+        total: parseInt(data.product1Val),
+      },
+      {
+        image: "https://www.mobbex.com/wp-content/uploads/2019/03/web_logo.png",
+        quantity: 1,
+        description: data.product2Title,
+        total: parseInt(data.product2Val),
+      },
+    ],
+    customer: config.customer,
+    options: {
+      domain: config.domain,
+    },
+    test: true,
+    wallet: true,
+    return_url: "https://localhost:8443/sale/return?session=56789",
+    webhook: "https://localhost:8443/sale/webhook?user=1234",
+  };
 
-            // Obtener la imagén de la tarjeta (visa, mastercard...)
-            wallet.forEach(source => {               
-                let logo = source.source.card.product.logo
-                // Se coloca la imagén en el source para poder renderizalo en el frontend
-                source.logo = logo
-            })
-            res.render('checkout', {wallet: wallet, url: url})
-        })
-        .catch(error => console.log(error))
-})
+  // Creación del checkout
+  mobbex.checkout
+    .create(checkout)
+    .then((data) => {
+      // Wallet con todas las tarjetas
+      let wallet = data.data.wallet;
+      // Url del checkout para agregar nuevas tarjetas
+      let url = data.data.url;
 
-app.listen(config.port)
+      // Obtener la imagén de la tarjeta (visa, mastercard...)
+      wallet.forEach((source) => {
+        let logo = source.source.card.product.logo;
+        // Se coloca la imagén en el source para poder renderizalo en el frontend
+        source.logo = logo;
+      });
+      res.render("checkout", { wallet: wallet, url: url });
+    })
+    .catch((error) => console.log(error));
+});
 
-console.log(`Running on port: ${config.port}`)
+app.listen(config.port);
+
+console.log(`Running on port: ${config.port}`);
